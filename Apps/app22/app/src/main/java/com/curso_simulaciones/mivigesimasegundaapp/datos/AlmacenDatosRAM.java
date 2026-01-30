6c3a7e5b808948820c1756969320ef4de0ee65fc
mrioshe <143111391+mrioshe@users.noreply.github.com>
2026-01-29 21:24:34 -0500

    MiVigesimaSegundaApp first try

---
+++ Apps/app22/app/src/main/java/com/curso_simulaciones/mivigesimasegundaapp/datos/AlmacenDatosRAM.java
@@ -0,0 +1,12 @@
+package com.curso_simulaciones.mivigesimasegundaapp.datos;
+
+public class AlmacenDatosRAM {
+
+    public static float ancho_pantalla, alto_pantalla;
+
+
+
+    public AlmacenDatosRAM() {
+
+    }
+}
