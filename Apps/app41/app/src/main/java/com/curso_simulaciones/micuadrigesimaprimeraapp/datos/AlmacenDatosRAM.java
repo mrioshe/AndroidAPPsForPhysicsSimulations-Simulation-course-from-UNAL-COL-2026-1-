b6a221ed7c92742148c639be3657c00e6ee136ea
mrioshe <mrioshe@unal.edu.co>
2026-03-18 18:52:11 -0500

    App41 done

---
+++ Apps/app41/app/src/main/java/com/curso_simulaciones/micuadrigesimaprimeraapp/datos/AlmacenDatosRAM.java
@@ -0,0 +1,20 @@
+package com.curso_simulaciones.micuadrigesimaprimeraapp.datos;
+
+public class AlmacenDatosRAM {
+
+
+    public static int ancho, alto, dimensionReferencia, tamanoLetraResolucionIncluida;
+
+
+    public static String MQTTHOST;
+    public static String USERNAME;
+    public static String PASSWORD;
+    public static String topicStr;
+
+    public static String conectado_PubSub=" ";
+
+    public static boolean conectado=false;
+
+
+}
+
